11c0e754d718744c373585c5ef5cb6ed7a366241
TylerDurden75 <raytibo.j@gmail.com>
2021-09-04 18:07:01 +0200

    nav on menu-items + refactoring code SpreadOp

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,6 +13,7 @@ class Directory extends React.Component {
           title: "hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
+          linkUrl: "hats",
         },
         {
           title: "jackets",
@@ -23,18 +24,21 @@ class Directory extends React.Component {
           title: "sneakers",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3,
+          linkUrl: "",
         },
         {
           title: "womens",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
           id: 4,
+          linkUrl: "",
         },
         {
           title: "mens",
           imageUrl: "https://i.ibb.co/R70vBrQ/mens.png",
           size: "large",
           id: 5,
+          linkUrl: "",
         },
       ],
     };
@@ -43,8 +47,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
