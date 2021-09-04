7e1d0f99ba9d27d4e35c2968278a7d0117ea021f
TylerDurden75 <raytibo.j@gmail.com>
2021-09-04 20:19:08 +0200

    functionnal form/button custom and reusable

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/header/header.component";
-import HomePage from "./pages/homepage/homepage.component.jsx";
-import ShopPage from "./pages/shop/shop.component.jsx";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SigninAndSignupPage from "./pages/signin-and-signup/signin-and-signup.component";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SigninAndSignupPage} />
       </Switch>
     </div>
   );
