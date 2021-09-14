39b0d646cb0f579e5d2b9b0c54f70e68b40f03c9
TylerDurden75 <raytibo.j@gmail.com>
2021-09-14 17:34:18 +0200

    stripe implement update + updated favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JZcwMAdDdBNw3JeCMHIODwv9z9YLczwNBwEQlPuERBZev04KMRxC8e1iSHBnNhC8B5SeYAUb2jb60frjq4P1v3w00eomy0Zxt";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successfull");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Ecom Test T.D"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
