6d6757f9f2e0854f586cfdd15157f624465a0a6b
TylerDurden75 <raytibo.j@gmail.com>
2021-09-21 20:28:17 +0200

    fixing styles scss for shop components

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -7,7 +7,7 @@ import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import CollectionPreview from "../preview-collection/collection-preview.component";
 
 const CollectionsOverview = ({ collections }) => (
-  <div className="collection-overview">
+  <div className="collections-overview">
     {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -15,7 +15,7 @@ const CollectionPage = () => {
   return (
     <div className="collection-page">
       <h2 className="title">{title}</h2>
-      <div className="item">
+      <div className="items">
         {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
