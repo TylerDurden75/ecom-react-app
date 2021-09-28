b718037251db4973a34b42c9df596804e411710e
TylerDurden75 <raytibo.j@gmail.com>
2021-09-28 14:29:46 +0200

    deploying on heroku

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-import express from "express";
-const bodyParser = require("body-parser");
-const path = require("body-parser");
+import express, { static } from "express";
+import { json, urlencoded, path } from "body-parser";
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
@@ -9,14 +8,14 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(join(__dirname, "client/build")));
+  app.use(static(path.join(__dirname, "client/build")));
 
   app.get("*", function (req, res) {
-    res.sendFile(join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
