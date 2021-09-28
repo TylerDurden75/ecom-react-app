8feb8822e08bbd11ef7a70eaf6b3dc66b1fa6e28
TylerDurden75 <raytibo.j@gmail.com>
2021-09-28 14:53:34 +0200

    deploying on heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("body-parser");
+const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
@@ -13,10 +13,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname, "client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function (req, res) {
-    res.sendFile(__dirname, "client/build", "index.html");
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
