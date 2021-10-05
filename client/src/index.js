b5950815e2c6ec16876a3e5ef17d2ea61797a80b
TylerDurden75 <raytibo.j@gmail.com>
2021-10-05 14:44:29 +0200

    updating code to be PWA compliant with service worker, https, manifest icon

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { store, persistor } from "./redux/store";
+import * as serviceWorker from "./service-worker";
 
 import "./index.css";
 import App from "./App";
@@ -21,6 +22,8 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+serviceWorker.register();
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
